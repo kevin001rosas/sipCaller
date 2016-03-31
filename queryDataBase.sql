febac2975003356e65de425a97fe441ece53aa66
Kevin941 <kevin_brightside@hotmail.com>
2016-03-31 13:01:35 -0600

    Actualizacion del query.

--- queryDataBase.sql
+++ queryDataBase.sql
@@ -7,7 +7,7 @@ CREATE TABLE `t_usuarios` (
   `correo` varchar(255) DEFAULT NULL,
   `extension` varchar(255) DEFAULT NULL,  
   PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 CREATE TABLE `t_inventarios` (
   `idInventario` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
@@ -15,25 +15,25 @@ CREATE TABLE `t_inventarios` (
   `fechaCompra` varchar(255) DEFAULT NULL,
   `factura` varchar(255) DEFAULT NULL,  
   PRIMARY KEY (`idInventario`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `t_reportes` (
   `idReporte` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `id_categoría` varchar(255) DEFAULT NULL,
+  `id_categoria` varchar(255) DEFAULT NULL,
   `extension` varchar(255) DEFAULT NULL,
   `num_inventario` varchar(255) DEFAULT NULL,
   `problema` varchar(255) DEFAULT NULL,  
   `fecha_repo` varchar(255) DEFAULT NULL,  
   `fecha_cierra` varchar(255) DEFAULT NULL,  
-  `solución` varchar(255) DEFAULT NULL,  
-  `calificación` varchar(255) DEFAULT NULL, 
+  `solucion` varchar(255) DEFAULT NULL,  
+  `calificacion` varchar(255) DEFAULT NULL, 
   `abierto` bool default true, 
   PRIMARY KEY (`idReporte`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 	
 CREATE TABLE `t_categorias` (
   `idCategoria` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `descripcion` varchar(255) DEFAULT NULL,  
-  PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
